3c7d118b292ffd01fe21f6cc22721d83b550b0a4
IoannouJohn <150496551+IoannouJohn@users.noreply.github.com>
2023-11-11 15:39:37 +1100

    Update tt.js

--- tt.js
+++ tt.js
@@ -3,6 +3,12 @@ maxMult = 20
 mult = 1
 minMult = 2
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function GetAns (a, b){
     while(true){
         const input = prompt();
@@ -16,11 +22,53 @@ function GetAns (a, b){
         //     return asInt
     }
 }
+        
+function NewMult(old){
+    
+    NewMult = getRandomInt(minMult, maxMult)
 
-function init (){
-    console.log("Hello world!")
+    while (NewMult == old){
+        NewMult = getRandomInt(minMult, maxMult)
+    }
+        return NewMult
+}
+
+function question(testNums, mult){
+    
+    testNum = testNums[getRandomInt(1, testNums.length - 1)]
+    ans = testNum * mult
+    usrAns = GetAns(testNum, mult)
+    
+    console.log(`${testNum} X ${mult} = ${ans}`)
+
     var msg = new SpeechSynthesisUtterance();
     msg.text = "What is 17 times 13";
     window.speechSynthesis.speak(msg);
-    GetAns(1,2)
+
+
+    while (true){
+        if (usrAns != ans){
+            print("!", end="")
+            usrAns = GetAns(testNum, mult)
+        }
+        else{ 
+            return
+        }        
+    }
+}
+
+
+function init (){
+
+    // console.log("Hello world!")
+    // for (let i = 0; i < 100; i++) {
+    //     console.log(getRandomInt(1, 20))
+    // }
+
+
+    while (true){
+        mult = NewMult(mult)
+        question(testNums, mult)    
+
+    }
 }
