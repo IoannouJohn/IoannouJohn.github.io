6b29052ff5b1cd06286187f631b377732eef127c
IoannouJohn <150496551+IoannouJohn@users.noreply.github.com>
2023-12-29 15:09:53 +1100

    changed randomness

--- drop.js
+++ drop.js
@@ -15,15 +15,42 @@ function SayQuestion(n) {
     window.speechSynthesis.speak(msg);
 }
 
-async function init(){
+function GetNonLinearRandom() {
+    n = getRandomInt(1, 100)
+    if (n <= 10)
+        return 1;
+    if (n <= 20)
+        return 2;
+    if (n <= 40)
+        return 3;
+    if (n <= 70)
+        return 4;
+    return 5;
 
-    while(true){
-        n = getRandomInt(1,5)
+
+}
+
+
+async function init() {
+    log = [0,0,0,0,0]
+    while (true) {
+        n = GetNonLinearRandom();
         SayQuestion(n)
         sleepLength = parseInt(document.getElementById("id").value)
         await sleep(sleepLength);
-
-        console.log(n)
-        console.log(sleepLength/1000)
+        log[n - 1] = log[n - 1] + 1
+        
+        // console.log(log)
+        
+        // console.log(n)
+        // console.log(sleepLength / 1000)
     }
 }
+
+
+
+// 1 10
+// 2 10
+// 3 20
+// 4 30
+// 5 30 
